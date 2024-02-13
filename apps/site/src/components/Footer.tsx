70c6b49f6bf771552f539ba27cddd0d48cbbc7ac
Daniel James <danielcolinjames@gmail.com>
2024-02-13 02:32:18 -0500

    style fixes

--- apps/site/src/components/Footer.tsx
+++ apps/site/src/components/Footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
     return (
 
         <div className="fixed bottom-0 px-4 py-4 bg-black-60 backdrop-blur-lg">
-            <p className="text-center text-sm text-gray-600">
+            <p className="text-center text-xs text-gray-600">
                 fly.town is not endorsed by or associated with Blackbird Labs Inc.
             </p>
             {/* <a className="text-gray-500 text-xs" href="https://x.com/dcwj" target="_blank" rel="noopener noreferrer">(made by a fan)</a> */}
