ffaab9338a8cf0644976e43026c2a970be836eba
Daniel James <danielcolinjames@gmail.com>
2024-02-13 02:27:47 -0500

    add meta tags and favicon

--- apps/site/src/app/layout.tsx
+++ apps/site/src/app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
   title: 'fly.town',
   description: 'What’s the FLYcast today?',
   openGraph: {
-    images: 'https://fly.town/og.png',
+    images: 'https://fly.town/og2.png',
   },
 }
 
@@ -28,6 +28,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={satoshiFont.variable}>
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <meta name="msapplication-TileColor" content="#000000" />
+      <meta name="theme-color" content="#ffffff" />
       <body>
         {children}
         <Analytics />
