97f635c584c8d3aa1e8b843269c1f9d0d17f03b0
Daniel James <danielcolinjames@gmail.com>
2024-02-17 01:50:36 -0500

    fix styling

--- apps/site/src/components/Footer.tsx
+++ apps/site/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <div className="flex flex-row gap-1 sm:gap-2 items-center justify-center fixed w-full bottom-0 pb-1 sm:py-2 bg-black/60 backdrop-blur-lg">
-      <a className="text-gray-500 text-[10px] sm:text-xs min-w-[28px]" href="https://x.com/dcwj" target="_blank" rel="noopener noreferrer">by dcj</a>
+      <a className="text-gray-500 text-[8px] sm:text-xs min-w-[28px]" href="https://x.com/dcwj" target="_blank" rel="noopener noreferrer">by dcj</a>
       <p className="hidden sm:shown text-center text-xs text-gray-800">
         |
       </p>
