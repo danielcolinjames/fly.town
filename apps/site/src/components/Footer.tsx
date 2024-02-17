43cb83564c18fd021dbb2d710f4964d91337e649
Daniel James <danielcolinjames@gmail.com>
2024-02-17 01:58:50 -0500

    more style

--- apps/site/src/components/Footer.tsx
+++ apps/site/src/components/Footer.tsx
@@ -1,8 +1,8 @@
 export const Footer = () => {
   return (
-    <div className="flex flex-row gap-1 sm:gap-2 items-center justify-center fixed w-full bottom-0 py-1 sm:py-2 bg-black/60 backdrop-blur-lg">
-      <a className="text-[#76BEFB] text-[8px] sm:text-xs min-w-[28px] opacity-30" href="https://x.com/dcwj" target="_blank" rel="noopener noreferrer">by dcj</a>
-      <p className="hidden sm:shown text-center text-xs text-gray-800">
+    <div className="flex flex-row items-center justify-center fixed w-full bottom-0 py-1 bg-black/60 backdrop-blur-lg gap-2">
+      <a className="text-[#76BEFB] text-[8px] sm:text-xs opacity-30" href="https://x.com/dcwj" target="_blank" rel="noopener noreferrer">by dcj</a>
+      <p className="text-center text-xs text-gray-600">
         |
       </p>
       <p className="text-center text-[8px] sm:text-xs text-gray-600 opacity-60">
