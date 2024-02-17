360e1f9cb563d2082bed931f0a3e958f58ef0a0f
Daniel James <danielcolinjames@gmail.com>
2024-02-17 01:48:45 -0500

    footer style unfuck

--- apps/site/src/components/Footer.tsx
+++ apps/site/src/components/Footer.tsx
@@ -1,11 +1,11 @@
 export const Footer = () => {
   return (
-    <div className="flex flex-row gap-2 items-center justify-center fixed w-full bottom-0 py-2 bg-black/60 backdrop-blur-lg">
-      <a className="text-gray-600 text-xs" href="https://x.com/dcwj" target="_blank" rel="noopener noreferrer">made by dcj</a>
-      <p className="text-center text-xs text-gray-800">
+    <div className="flex flex-row gap-1 sm:gap-2 items-center justify-center fixed w-full bottom-0 pb-1 sm:py-2 bg-black/60 backdrop-blur-lg">
+      <a className="text-gray-500 text-[10px] sm:text-xs min-w-[28px]" href="https://x.com/dcwj" target="_blank" rel="noopener noreferrer">by dcj</a>
+      <p className="hidden sm:shown text-center text-xs text-gray-800">
         |
       </p>
-      <p className="text-center text-xs text-gray-600">
+      <p className="text-center text-[8px] sm:text-xs text-gray-600">
         fly.town is not endorsed by or associated with Blackbird Labs Inc.
       </p>
     </div>
