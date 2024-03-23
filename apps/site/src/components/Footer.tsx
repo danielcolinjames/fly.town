43dee0f2ce4b4a597c0056ad3fdf8f2e9b8edb5f
Daniel James <danielcolinjames@gmail.com>
2024-03-23 16:01:28 -0400

    simplify footer

--- apps/site/src/components/Footer.tsx
+++ apps/site/src/components/Footer.tsx
@@ -3,7 +3,7 @@ export const Footer = () => {
     <div className="flex flex-col items-center justify-center w-full absolute bottom-0 py-2 sm:py-4 bg-black/60 backdrop-blur-lg">
       <div className="flex flex-row items-center justify-center w-full gap-2">
         <a
-          className="text-brandYellow font-light text-[8px] sm:text-xs opacity-60"
+          className="text-gray-300 font-light text-[8px] sm:text-xs opacity-60"
           href="https://x.com/dcwj"
           target="_blank"
           rel="noopener noreferrer"
@@ -15,10 +15,6 @@ export const Footer = () => {
           fly.town is not endorsed by or associated with Blackbird Labs Inc.
         </p>
       </div>
-      <p className="text-center text-[8px] sm:text-xs text-[#727272] px-4">
-        if things seem jank or broken, they definitely are :) please tag @dcj on
-        Discord if you have ideas for ways to make fly.town better
-      </p>
     </div>
   );
 };
